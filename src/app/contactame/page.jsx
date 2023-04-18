6342b378b8169943492ffaf0ef66569e7e00d8bb
Mealvarezm <enriquemerlos49@gmail.com>
2023-04-18 01:16:43 -0600

    arreglando bug

--- src/app/contactame/page.jsx
+++ src/app/contactame/page.jsx
@@ -7,6 +7,7 @@ import { BsFillEnvelopeFill } from "react-icons/bs";
 import { BsFillFileEarmarkPersonFill } from "react-icons/bs";
 import { BiAlignMiddle } from "react-icons/bi";
 
+
 const Contactame =()=>{
 return (
     <>
@@ -26,23 +27,23 @@ return (
                     
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contactame</h2>
                     <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">¿Tienes un problema técnico o necesitas ayuda con algún proyecto en particular? ¡Haznos saber! Estamos aquí para ayudarte.</p>
-                <form action="https://formsubmit.co/malvarezm26@miumg.edu.gt" method="POST" className="space-y-8 ">
+                <form action="./form.php" method="POST" className="space-y-8 ">
                      
                          <div>
-                            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">Nombre<BsFillFileEarmarkPersonFill className="flex mx-auto text-[#3fccebfa]"/></label>
-                            <input type="text" name="nombre" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 textt-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " placeholder="Tu nombre" required></input>
+                            <label for="name" className="block mb-2 text-sm font-medium text-gray-900 ">Nombre<BsFillFileEarmarkPersonFill className="flex mx-auto text-[#3fccebfa]"/></label>
+                            <input type="text" name="name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 textt-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " placeholder="Tu nombre" required></input>
                         </div>
                         <div>
                             <label for="email" className="block mb-2 text-sm font-medium text-gray-900 " >Tu correo<SiWebmoney className="flex mx-auto text-[#3fccebfa]"/></label>
                             <input type="text" name="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 textt-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 " placeholder="Usuario@gmail.com" required></input>
                         </div>
                         <div>
-                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">Asunto<BiAlignMiddle className="flex mx-auto text-[#3fccebfa]"/></label>
+                        <label for="asunto" className="block mb-2 text-sm font-medium text-gray-900 ">Asunto<BiAlignMiddle className="flex mx-auto text-[#3fccebfa]"/></label>
               <input type="text" name="asunto" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Permítenos saber en qué te podemos ayudar" required></input>
                         </div>
                         <div class="sm:col-span-2">
               <label for="endaje" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Tu mensaje<BsFillEnvelopeFill className="flex mx-auto text-[#3fccebfa]"/></label>
-              <textarea name="comentario" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Deja un comentario..."></textarea>
+              <textarea name="textarea" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Deja un comentario..." required></textarea>
              </div>
              <button type="submit" value="Enviar" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 bg-[#3fccebfa] hover:bg-[#67c8e6fa]">Enviar<BsFillCursorFill className="flex mx-auto text-[#fffffffa]"/></button>
                     </form> 
